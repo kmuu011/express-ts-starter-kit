54424fd0beea65c1d0a7b394aa432c0318accde5
강민우 <kmuu011@naver.com>
2025-04-30 13:58:13 +0900

    무중단 배포 명령어 일부 수정

--- controller_for_server/initial.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SHELL_PATH=$(dirname $0)
-
-cd $SHELL_PATH
-
-cd ..
-cd server
-
-npm ci --force
--- controller_for_window/release-server.bat
+++ controller_for_window/release-server.bat
@@ -2,7 +2,10 @@ cd ..
 cd docker-compose
 cd express_ts_starter_kit_production
 
-docker compose down node_0
-docker compose up node_0 -d
-docker compose down node_1
-docker compose up node_1 -d
+docker compose stop node_0
+docker compose rm -f node_0
+docker compose up -d --no-deps node_0
+
+docker compose stop node_1
+docker compose rm -f node_1
+docker compose up -d --no-deps node_1
